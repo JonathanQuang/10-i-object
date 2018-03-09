78cc5a4a1722932605a5cd641abb3bbbd73f28ea
JonathanQuang <jquang@stuy.edu>
2018-03-08 23:45:15 -0500

    finished commit

--- circle.js
+++ circle.js
@@ -1,25 +1,39 @@
 console.log("loaded");
 svg = document.getElementById("svgElement");
+idCounter = 0;
 
 circleSpace = [];
 
-var insertCircle = function(x,y){
+var insertCircle = function(x,y,id){
 
 	theCircle = document.createElementNS("http://www.w3.org/2000/svg","circle");
 	theCircle.setAttribute("cx",x.toString());
-	theCircle.setAttribute("cy",e.offsetY.toString());
+	theCircle.setAttribute("cy",y.toString());
 	theCircle.setAttribute("r","25");
+	theCircle.setAttribute("id",id);
 	theCircle.addEventListener("click",colorCircle);
 	svg.append(theCircle);
+	console.log(theCircle);
 }
 
-var newCircle = function(x,y){
+var makeChanges = function(){
+	relevantCircle = document.getElementById(this.id.toString());
+	relevantCircle.setAttribute("cx",this.x.toString());
+	relevantCircle.setAttribute("cy",this.y.toString());
+	relevantCircle.setAttribute("fill",this.color);
+}
+
+var newCircle = function(x,y,id){
 	retCircle = {
+		"id":id,
 		"x":x,
 		"y":y,
 		"color":"black",
-		"createCircle":insertCircle,
+		"createCircle":function(cx,cy){insertCircle(cx,cy,this.id);},
+		"renderChanges":makeChanges,
 	};
+	circleSpace.push(retCircle);
+	return retCircle;
 }
 
 
@@ -28,17 +42,22 @@ var createCircle = function(e){
 	console.log('create');
 	theCircle = document.createElementNS("http://www.w3.org/2000/svg","circle");
 
-	theCircle.setAttribute("cx",e.offsetX.toString());
-	theCircle.setAttribute("cy",e.offsetY.toString());
-	theCircle.setAttribute("r","25");
-	theCircle.addEventListener("click",colorCircle,true);
-	svg.appendChild(theCircle);
-	e.stopPropagation();
+	idCounter +=1;
+	newCirc = newCircle(e.offsetX,e.offsetY,idCounter);
+	newCirc.createCircle(newCirc.x,newCirc.y);
+	
 }
 
 var colorCircle = function(e){
 	console.log('color change');
-	this.setAttribute("fill","red");
+	relevantCircle = null;
+	for (i = 0; i < circleSpace.length; i++){
+		if (this.id == circleSpace[i].id.toString()){
+			relevantCircle = circleSpace[i];
+		}
+	}
+	relevantCircle.color="red";
+	relevantCircle.renderChanges();
 	e.stopPropagation();
 	this.addEventListener("click",removeCircle,true);
 	this.removeEventListener('click',colorCircle);
@@ -46,19 +65,23 @@ var colorCircle = function(e){
 
 var removeCircle = function(e){
 	console.log('remove');
+	e.stopPropagation();
+	relevantCircleIndex = -1;
+	for (i=0;i<relevantCircleIndex.length;i++){
+		if (this.id == circleSpace[i].id.toString()){
+			relevantCircleIndex=i;
+		}
+	}
+
+
+	circleSpace = circleSpace.splice(relevantCircleIndex,1);
 	svg.removeChild(this);
-	e.stopPropagation();
-
 	
-	theCircle.setAttribute("cx",Math.random()*500);
-	theCircle.setAttribute("cy",Math.random()*500);
-	theCircle.setAttribute("r","25");
-	theCircle.setAttribute("fill","black");
-	theCircle.addEventListener("click",colorCircle,true);
-	e.stopPropagation();
-	svg.append(theCircle);
-	
+	idCounter++;
+	randX = Math.random()*500;
+	randY = Math.random()*500;
+	theCircle = newCircle(randX,randY,idCounter);
+	theCircle.createCircle(theCircle.x,theCircle.y);
 }
-
 
 document.getElementById("svgElement").addEventListener("click",createCircle);
